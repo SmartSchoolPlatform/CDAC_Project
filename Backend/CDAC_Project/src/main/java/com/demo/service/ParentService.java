fc3cd988dbc57809887106733bc64ad952eccf7b
pragati_waghmare <165298541+pragati-waghmare@users.noreply.github.com>
2024-08-12 13:14:19 +0530

    ParentService

---
+++ Backend/CDAC_Project/src/main/java/com/demo/service/ParentService.java
@@ -0,0 +1,12 @@
+package com.demo.service;
+
+import com.demo.beans.Parent;
+import java.util.List;
+
+public interface ParentService {
+    List<Parent> getAllParents();
+    Parent getParentById(Long parentId);
+    Parent saveParent(Parent parent);
+    void deleteParent(Long parentId);
+	List<Long> getClassIdsByParentId(Long parentId);
+}
